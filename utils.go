e609af785e820081fbf0b1369bcb02eb63541d95
Fuad <justfuad@protonmail.com>
2024-09-05 16:18:10 +0300

    add refresh todo list helper

--- utils.go
+++ utils.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"io"
 	"log"
 	"os"
+
+	"fyne.io/fyne/v2/widget"
 )
 
 func imageToBytes(imagePath string) ([]byte, error) {
@@ -21,4 +25,28 @@ func imageToBytes(imagePath string) ([]byte, error) {
 
 	return imageBytes, nil
 
+}
+
+func refreshTodoList(db *sql.DB, list *widget.Table) error {
+	err := deletePastDueTodos(db)
+	if err != nil {
+		return err
+	}
+
+	todos, err := listTodos(db)
+	if err != nil {
+		return err
+	}
+
+	listTodoView = [][]string{{"ID", "Title", "Deadline"}}
+	for _, todo := range todos {
+		listTodoView = append(listTodoView, []string{
+			fmt.Sprintf("%d", todo.ID),
+			todo.Title,
+			todo.Deadline,
+		})
+	}
+
+	list.Refresh()
+	return nil
 }
